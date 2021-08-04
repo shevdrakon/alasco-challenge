5be69b1f1cf61ae03de78d1a827505bdb0cc786b
Vadim Sheydakov <vadim.sheydakov@alasco.de>
2021-08-04 16:10:26 +0200

    actions/core

--- .github/pr-bot/index.js
+++ .github/pr-bot/index.js
@@ -1,1 +1,7 @@
+import * as core from '@actions/core'
+import {context, GitHub} from '@actions/github'
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 console.log("HI!!!");
